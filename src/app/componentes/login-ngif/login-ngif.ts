59b252e8b0d829faa677f00ec6ee1ae012a2c7a8
Murilo Elias Miranda <muriloelias15@gmail.com>
2026-09-22 22:03:01 -0300

    Adicionando logado e não logado

--- src/app/componentes/login-ngif/login-ngif.ts
+++ src/app/componentes/login-ngif/login-ngif.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { NgClass, NgIf } from '@angular/common';
 
 @Component({
-  imports: [],
+  imports: [NgClass, NgIf],
   selector: 'app-login-ngif',
   styleUrl: './login-ngif.css',
   templateUrl: './login-ngif.html',
 })
-export class LoginNgif {}
+export class LoginNgif {
+  logado:boolean = false;
+  login:string = '';
+  senha:string = '';
+  logar() {
+    this.logado = this.login === 'admin' && this.senha === 'admin';
+  }
+
+  deslogar() {
+    this.logado = false;
+  }
+}
